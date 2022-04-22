8638fcd0caf441a7552a091eaac2e718b809bbb7
Nathan Hamley <nathan.hamley@peakwork.com>
2022-04-22 11:14:54 +0200

    make network select it's own component

---
+++ src/components/NetworkSelect.vue
@@ -0,0 +1,86 @@
+<template>
+  <div>
+    <q-select dense dark rounded outlined v-model="selectedNetwork" :options="networkOptions"
+      @update:model-value="networkSelectionChanged">
+      <template v-slot:prepend>
+        <q-icon class="text-white" size="xs" name="mdi-web" />
+      </template>
+      <template v-slot:option="scope">
+        <q-item v-bind="scope.itemProps" :class="scope.opt.value === undefined ? 'primary-clickable' : ''">
+          <q-item-section>
+            <q-item-label>
+              {{ scope.opt.label }}
+            </q-item-label>
+          </q-item-section>
+          <q-item-section v-if="canBeDeleted(scope.opt.value, scope.selected)" side top>
+            <q-btn @click.stop="removeNetworkOption(scope.index)" round size="sm" color="primary"
+              icon="mdi-delete-forever" />
+          </q-item-section>
+        </q-item>
+      </template>
+    </q-select>
+    <AddNetworkDialog v-model="showNewNetworkDialog" @new-network="createNewNetwork" />
+  </div>
+</template>
+
+<script lang="ts">
+import { useAppConfig } from 'src/stores/app-config';
+import { Network } from 'src/types/network'
+import { defineComponent, ref } from 'vue';
+import AddNetworkDialog from 'src/components/dialogs/AddNetworkDialog.vue';
+
+function createNetworkOptions(networks: Network[]): Array<{ label: string, value: Network | undefined }> {
+  const networkOptions = networks.map(network => ({
+    label: network.displayName,
+    value: network as Network | undefined
+  }));
+  networkOptions.push({ label: 'Create new Network', value: undefined });
+  return networkOptions
+}
+
+export default defineComponent({
+  name: 'NetworkSelect',
+  setup() {
+    const appConfig = useAppConfig();
+    const showNewNetworkDialog = ref(false);
+    const networkOptions = ref(createNetworkOptions(appConfig.getAllNetworks));
+    const selectedNetwork = ref(networkOptions.value.find(e => e.value === appConfig.getActive) || networkOptions.value[0])
+    return {
+      networkOptions,
+      selectedNetwork,
+      showNewNetworkDialog,
+      networkSelectionChanged(selectedOption: { label: string, value: Network }) {
+        if (selectedOption.value === undefined) {
+          showNewNetworkDialog.value = true;
+        } else {
+          //TODO handle sucess/not success!
+          appConfig.setActive(selectedOption.value.id)
+        }
+      },
+      createNewNetwork(value: Network) {
+        appConfig.pushNetwork(value);
+        appConfig.setActive(value.id);
+        selectedNetwork.value = { label: value.displayName, value: value }
+        networkOptions.value.splice(networkOptions.value.length - 1, 0, { label: value.displayName, value: value })
+        showNewNetworkDialog.value = false;
+      },
+      removeNetworkOption(index: number) {
+        const network = networkOptions.value[index];
+        if (network.value === undefined) {
+          return;
+        }
+        appConfig.removeNetwork(network.value.id)
+        networkOptions.value = createNetworkOptions(appConfig.getAllNetworks);
+      },
+      canBeDeleted(value: Network, selected: boolean) {
+        return value !== undefined && !value.predefined && !selected
+      }
+    }
+  },
+  components: { AddNetworkDialog }
+})
+</script>
+<style scoped lang="sass">
+.primary-clickable
+  background-color: $primary
+</style>
--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -16,26 +16,7 @@
             {{ route?.name }} </router-link>
           <a class="text-white q-mr-md no-underscore" v-for="link in additionalMenuItems" :key="link.name"
             :href="link.href" target="_blank">{{ link.name }}</a>
-          <q-select dense dark rounded outlined v-model="selectedNetwork" :options="networkOptions"
-            @update:model-value="networkSelectionChanged">
-            <template v-slot:prepend>
-              <q-icon class="text-white" size="xs" name="mdi-web" />
-            </template>
-            <template v-slot:option="scope">
-              <q-item v-bind="scope.itemProps" :class="scope.opt.value === undefined ? 'test' : ''">
-                <q-item-section>
-                  <q-item-label>
-                    {{ scope.opt.label }}
-                  </q-item-label>
-                </q-item-section>
-                <q-item-section v-if="canBeDeleted(scope.opt.value, scope.selected)" side top>
-                  <q-btn @click.stop="removeNetworkOption(scope.index)" round size="sm" color="primary"
-                    icon="mdi-delete-forever" />
-                </q-item-section>
-              </q-item>
-            </template>
-
-          </q-select>
+          <network-select />
         </div>
       </q-toolbar>
     </q-header>
@@ -57,7 +38,6 @@
       </q-scroll-area>
     </q-drawer>
 
-    <AddNetworkDialog v-model="showNewNetworkDialog" @new-network="createNewNetwork" />
 
     <body>
       <q-page-container>
@@ -74,9 +54,8 @@ import { defineComponent, ref } from 'vue'
 import { useI18n } from 'vue-i18n';
 import { RouteRecordRaw, useRouter } from 'vue-router';
 import type { ExternalMenuLink } from 'src/types/external-menu-link';
-import AddNetworkDialog from 'src/components/dialogs/AddNetworkDialog.vue';
-import { useAppConfig } from 'src/stores/app-config';
-import { Network } from 'src/types/network';
+
+import NetworkSelect from 'src/components/NetworkSelect.vue'
 
 const leftDrawerOpen = ref(false)
 
@@ -87,21 +66,12 @@ function addAllChildRoutes(gathered: Array<RouteRecordRaw>, route: RouteRecordRa
   }
 }
 
-function createNetworkOptions(networks: Network[]): Array<{ label: string, value: Network | undefined }> {
-  const networkOptions = networks.map(network => ({
-    label: network.displayName,
-    value: network as Network | undefined
-  }));
-  networkOptions.push({ label: 'Create new Network', value: undefined });
-  return networkOptions
-}
-
 export default defineComponent({
   name: 'MainLayout',
   setup() {
     const { t } = useI18n();
     const router = useRouter();
-    const appConfig = useAppConfig();
+
     leftDrawerOpen.value = false;
     // Can be optimized to filter earlier and not gather all routes
     let allRoutes: Array<RouteRecordRaw> = [];
@@ -114,9 +84,6 @@ export default defineComponent({
       { name: 'Documentation', href: 'https://docs.camino.foundation' },
       { name: 'Wallet', href: 'https://wallet.camino.foundation' }
     ] as ExternalMenuLink[];
-    const showNewNetworkDialog = ref(false);
-    const networkOptions = ref(createNetworkOptions(appConfig.getAllNetworks));
-    const selectedNetwork = ref(networkOptions.value.find(e => e.value === appConfig.getActive) || networkOptions.value[0])
     return {
       leftDrawerOpen,
       toggleLeftDrawer() {
@@ -125,38 +92,9 @@ export default defineComponent({
       t,
       menuRoutes,
       additionalMenuItems,
-      networkOptions,
-      selectedNetwork,
-      showNewNetworkDialog,
-      networkSelectionChanged(selectedOption: { label: string, value: Network }) {
-        if (selectedOption.value === undefined) {
-          showNewNetworkDialog.value = true;
-        } else {
-          //TODO handle sucess/not success!
-          appConfig.setActive(selectedOption.value.id)
-        }
-      },
-      createNewNetwork(value: Network) {
-        appConfig.pushNetwork(value);
-        appConfig.setActive(value.id);
-        selectedNetwork.value = { label: value.displayName, value: value }
-        networkOptions.value.splice(networkOptions.value.length - 1, 0, { label: value.displayName, value: value })
-        showNewNetworkDialog.value = false;
-      },
-      removeNetworkOption(index: number) {
-        const network = networkOptions.value[index];
-        if (network.value === undefined) {
-          return;
-        }
-        appConfig.removeNetwork(network.value.id)
-        networkOptions.value = createNetworkOptions(appConfig.getAllNetworks);
-      },
-      canBeDeleted(value: Network, selected: boolean) {
-        return value !== undefined && !value.predefined && !selected
-      }
     };
   },
-  components: { AddNetworkDialog }
+  components: { NetworkSelect }
 })
 </script>
 <style scoped lang="sass">
@@ -169,6 +107,4 @@ a
   align-items: center
 .router-link-exact-active
   color: $primary !important
-.test
-  background-color: $primary
 </style>
