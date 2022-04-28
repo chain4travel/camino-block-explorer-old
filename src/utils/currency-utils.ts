ea1d0e7a4549d9a8d5082f284753aa39f28794a9
Marcus-Matthias Schütte <marcus.schuette@peakwork.com>
2022-04-28 17:06:04 +0200

    reduce conversion thresholds

--- src/utils/currency-utils.ts
+++ src/utils/currency-utils.ts
@@ -3,9 +3,9 @@ const conversionWeiPerCam = 1000000000000000000;
 
 const conversionWeiPerGwei = 1000000000;
 
-export const WEI_CAM_CONVERSION_THRESHHOLD = 100000000000000000;
+export const WEI_CAM_CONVERSION_THRESHHOLD = 1000000000000000;
 
-export const WEI_GWEI_CONVERSION_THRESHHOLD = 100000000;
+export const WEI_GWEI_CONVERSION_THRESHHOLD = 1000000;
 
 export function weiToCam(weiVal: number) {
   return weiVal / conversionWeiPerCam;
@@ -20,13 +20,16 @@ export function getDisplayValueForGewi(GweiVal: number): string {
 }
 
 export function getDisplayValue(weiVal: number): string {
+  if (weiVal === 0) {
+    return formatAmount(0, 'CAM')
+  }
   if (weiVal >= WEI_CAM_CONVERSION_THRESHHOLD) {
-    return formatAmount(weiToCam(weiVal), ' CAM');
+    return formatAmount(weiToCam(weiVal), 'CAM');
   }
   if (weiVal >= WEI_GWEI_CONVERSION_THRESHHOLD) {
-    return formatAmount(weiToGwei(weiVal), ' GWEI');
+    return formatAmount(weiToGwei(weiVal), 'GWEI');
   }
-  return formatAmount(weiVal, ' WEI');
+  return formatAmount(weiVal, 'WEI');
 }
 
 export function formatAmount(value: number, currency: string): string {
