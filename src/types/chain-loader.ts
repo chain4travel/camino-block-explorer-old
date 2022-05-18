ced7122d306e08b1c0f3e195a883e634574e5d55
Marcus-Matthias <marcus.schuette@peakwork.com>
2022-05-18 14:12:06 +0200

    remove todo

--- src/types/chain-loader.ts
+++ src/types/chain-loader.ts
@@ -1,4 +1,3 @@
-// TODO: rename methods and interface
 export interface ChainLoader {
   loadBlocks<T>(limit: number, offset: number): Promise<T[]>;
   loadTransactions<T>(limit: number, offset: number): Promise<T[]>;
