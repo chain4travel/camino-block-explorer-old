3941bf8bc0cb1203525da05e03362651b3cccc32
Ayagoumi <agoumi94@gmail.com>
2022-06-22 13:08:08 +0100

    Add: Logs tab in transactionDetailsView

--- src/pages/CChainTransactionDetails.vue
+++ src/pages/CChainTransactionDetails.vue
@@ -2,19 +2,47 @@
   <!-- content -->
   <div class="row">
     <div class="col-12">
-      <TransactionDetailsView
-        :type="type"
-        title="C-Chain Transaction"
-        :back-route="backroute"
-        :content="loadedTransaction"
-      >
-      </TransactionDetailsView>
+      <q-card class="col">
+        <q-card-section>
+          <div class="row">
+            <div class="col-auto">
+              <q-tabs
+                v-model="tab"
+                dense
+                class="text-grey"
+                active-color="primary"
+                indicator-color="primary"
+                align="justify"
+                narrow-indicator
+              >
+                <q-tab
+                  v-for="tab of tabs"
+                  :key="tab.key"
+                  :name="tab.key"
+                  :label="tab.label"
+                />
+              </q-tabs>
+            </div>
+          </div>
+          <q-tab-panels v-model="tab" animated>
+            <q-tab-panel name="transactions">
+              <TransactionDetailsView
+                :type="type"
+                title="C-Chain Transaction"
+                :back-route="backroute"
+                :content="loadedTransaction"
+              >
+              </TransactionDetailsView>
+            </q-tab-panel>
+          </q-tab-panels>
+        </q-card-section>
+      </q-card>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, ref } from 'vue';
 import TransactionDetailsView from 'src/components/TransactionDetailsView.vue';
 import { useRoute } from 'vue-router';
 import { getOverviewPath } from 'src/utils/route-utils';
@@ -22,13 +50,30 @@ import { ChainType } from 'src/types/chain-type';
 import { useCIndexStore } from 'src/stores/c-index-store';
 import { getStringOrFirstElement } from 'src/utils/display-utils';
 
+const tabs = [
+  {
+    key: 'transactions',
+    label: 'Transactions',
+  },
+  {
+    key: 'logs',
+    label: 'Logs',
+  },
+];
+
 export default defineComponent({
   name: 'CChainTransactionDetails',
   components: { TransactionDetailsView },
   async setup() {
     const route = useRoute();
     const cChain = useCIndexStore();
+    const ll = await cChain.loadTransactionById(
+      getStringOrFirstElement(route.params.transactionId)
+    );
+    console.log('llllllll', ll);
     return {
+      tab: ref('transactions'),
+      tabs,
       transactionId: route.params.transactionId,
       backroute: route.query.back
         ? route.query.back
