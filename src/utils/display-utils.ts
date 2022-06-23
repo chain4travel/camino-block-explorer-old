40025142150b4ccc2d50bdbf7107e75528597bd5
Ayoub Agoumi <agoumi94@gmail.com>
2022-06-24 00:22:50 +0100

    Fix: removal of the comments

--- src/utils/display-utils.ts
+++ src/utils/display-utils.ts
@@ -1,27 +1,7 @@
 import { Duration } from 'luxon';
 import { Fund } from 'src/types/transaction';
 
-// export function getRelativeTime(timestamp: Date | number | string): string {
-//   const time = getTime(timestamp);
-//   if (!Number.isInteger(time)) {
-//     return 'Unknown';
-//   }
-//   const duration = Duration.fromMillis(new Date().getTime() - time, {
-//     locale: 'en-US',
-//   }).shiftTo('seconds');
-//   if (duration.seconds < 1) {
-//     return 'less than one second';
-//   } else if (duration.seconds < 60) {
-//     return duration.toHuman({ maximumFractionDigits: 0 });
-//   } else if (duration.seconds < 3600) {
-//     return duration.shiftTo('minutes').toHuman({ maximumFractionDigits: 0 });
-//   } else if (duration.seconds < 86400) {
-//     return duration.shiftTo('hours').toHuman({ maximumFractionDigits: 0 });
-//   } else {
-//     return duration.shiftTo('days').toHuman({ maximumFractionDigits: 0 });
-//   }
-// }
-
+// Todo: Update the getRelativeTime function
 export function getRelativeTime(timestamp: Date | number | string): string {
   const time = getTime(timestamp);
   if (!Number.isInteger(time)) {
